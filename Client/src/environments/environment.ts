753e35fae551491297ca69e49062e80a1228a7b4
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2021-05-05 13:33:09 +0200

    Revert "[Data] Switch to SQLite"
    This reverts commit 137f2f836f6864e742921ce600942096620b4196.

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  apiUrl: 'https://webivrecipeapi.azurewebsites.net/api'
+  production: false,
+  apiUrl: '/api'
 };
 
 /*
